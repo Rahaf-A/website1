87fd615533c7986883ef4c826ce6416b5fecd638
Rahaf-A <116219713+Rahaf-A@users.noreply.github.com>
2024-02-27 22:47:03 +0000

    Tue, Feb 27, 2024, 2:47 PM -08:00

--- js/loading.js
+++ js/loading.js
@@ -1,5 +1,17 @@
-loadText = document.querySelector('.kiadubg-text')
+loadText = document.querySelector('.loading-text')
+bg = document.querySelector('.bg')
 
 load = 0
 
-loadText.innerText = "5%"
+int = setInterval(blurring, 30)
+
+function blurring() {
+    load = load + 1
+
+    if (load > 99){
+        clearInterval(int)
+    }
+    loadText.innerText = `${load}%`
+    loadText.style.opacity = 1
+    bg.style.filter = "blur(10px)"
+}
