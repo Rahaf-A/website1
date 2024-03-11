a7e1c8ad17e700ccc68f518c011032d80e0b3339
Rahaf-A <116219713+Rahaf-A@users.noreply.github.com>
2024-03-11 22:03:39 +0000

    Mon, Mar 11, 2024, 3:03 PM -07:00

--- js/landingPage.js
+++ js/landingPage.js
@@ -1,17 +1,28 @@
 toggle = document.getElementById('toggle')
 open = document.getElementById('open')
-modal = document.getElementById('model')
+model = document.getElementById('model')
 close = document.getElementById('close')
 
-
+// Toggel Nav
 toggle.addEventListener('click', () => {
     document.body.classList.toggle('show-nav')
 })
 
+// Show Modal
 open.addEventListener('click', () => {
     modal.classList.add('show-modal')
 })
 
+// Hide modal
 close.addEventListener('click', () => {
-    modal.classList.remove('show-modal')
+    modal.classList.remove('show-model')
 })
+
+// Hide Model on outside click
+window.addEventListener('click', (e) => {
+    if (e.target == modal) {
+        modal.classList.remove('show-modal')
+    }
+}
+
+)
