8246fdd8070f49933b861e3cbee0de906fae0e71
Rahaf-A <116219713+Rahaf-A@users.noreply.github.com>
2024-03-11 21:38:46 +0000

    Mon, Mar 11, 2024, 2:38 PM -07:00

--- js/landingPage.js
+++ js/landingPage.js
@@ -1,6 +1,12 @@
-toggle = document.getElementById('toggke')
+toggle = document.getElementById('toggle')
+open = document.getElementById('open')
+model = document.getElementId('model')
 
 
 toggle.addEventListener('click', () => {
     document.body.classList.toggle('show-nav')
+})
+
+open.addEventListener('click', () => {
+    modal.classList.add('show-mode')
 })
